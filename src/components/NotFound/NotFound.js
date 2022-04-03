5b7b5fbe54e71f6cffc2d84a56ba34ff6809292a
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-04-04 01:18:28 +0600

    notFound-part-finished

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import image from '../../Images/error.webp'
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>not found</h1>
+        <div className='error'>
+            <h2>Opps!!!! sorry
+                no data found
+            </h2>
+            <img src={image} alt="" />
         </div>
     );
 };
