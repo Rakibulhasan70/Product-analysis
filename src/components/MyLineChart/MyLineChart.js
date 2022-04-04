ce9921d2c0afef4046869c0a398526e091476cb5
Rakibulhasan70 <rakibulhasansohag5@gmail.com>
2022-04-04 12:19:20 +0600

    dashboard-finished

--- src/components/MyLineChart/MyLineChart.js
+++ src/components/MyLineChart/MyLineChart.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Area, AreaChart, Bar, BarChart, CartesianGrid, Line, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Line, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
 import { LineChart } from 'recharts';
 import './MyLineChart.css'
 
 const MyLineChart = () => {
-    const data = [
+    const data1 = [
         {
             "month": "Mar",
             "investment": 100000,
@@ -41,11 +41,49 @@ const MyLineChart = () => {
             "sell": 670,
             "revenue": 61000
         }
+    ];
+    const data2 = [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 200,
+            "revenue": 10201
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 400,
+            "revenue": 201500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 700,
+            "revenue": 263010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 500,
+            "revenue": 40605
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 600,
+            "revenue": 552900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 361050
+        }
     ]
     return (
         <div className='recharts'>
             <div>
-                <LineChart width={250} height={200} data={data}>
+                <LineChart width={250} height={200} data={data1}>
                     <Line dataKey={'sell'}></Line>
                     <XAxis dataKey='month'></XAxis>
                     <YAxis dataKey='sell'></YAxis>
@@ -53,7 +91,7 @@ const MyLineChart = () => {
                 </LineChart>
             </div>
             <div>
-                <BarChart width={250} height={200} data={data}>
+                <BarChart width={250} height={200} data={data1}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <Bar dataKey='month' fill="#8884d8"></Bar>
                     <Bar dataKey='investment' fill="#82ca9d"></Bar>
@@ -63,13 +101,20 @@ const MyLineChart = () => {
                 </BarChart>
             </div>
             <div>
-                <AreaChart width={300} height={200} data={data}>
+                <AreaChart width={300} height={200} data={data1}>
                     <CartesianGrid strokeDasharray="3 3" />
                     <Area dataKey='investment'></Area>
                     <Area dataKey='revenue' stroke="#82ca9d"></Area>
                     <XAxis dataKey='month' ></XAxis>
                     <YAxis data='investment'></YAxis>
                 </AreaChart>
+                <div>
+                    <PieChart width={300} height={200}>
+                        <Pie data={data1} dataKey="sell" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                        <Pie data={data2} dataKey="revenue" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                        <Tooltip></Tooltip>
+                    </PieChart>
+                </div>
             </div>
         </div>
     );
